bfba1cd9a3deceb07fda26bd8afaa31ea6bc290f
ganeshpalli77 <137283115+ganeshpalli77@users.noreply.github.com>
2024-05-11 08:35:33 +0530

    app.py

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ def find_cat_cont_columns(df): ## Logic to Separate Continuous & Categorical Col
 st.set_page_config(page_icon=":bar_chart:", page_title="Market Entry Strategy Optimization Tool")
 
 st.title("Market Entry Strategy Optimization Tool")
-st.caption("Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed. As a sample example, you can upload famous <a href='https://www.kaggle.com/competitions/titanic/data?select=train.csv'>Titanic Dataset</a> available from Kaggle.", unsafe_allow_html=True)
-st.markdown("Please feel free to visit <a href='https://www.youtube.com/watch?v=jS3uhxD-5Cs'>this video link</a> if you want to learn how to create this Web App.", unsafe_allow_html=True)
+st.caption("Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed.", unsafe_allow_html=True)
 upload = st.file_uploader(label="Upload File Here:", type=["csv"])
 
 if upload: ## File as Bytes
